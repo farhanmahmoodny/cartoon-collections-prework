97d08abd76051875f89b4dac2a40b9e8f420b01a
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-13 06:05:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,11 +7,13 @@ end
 def summon_captain_planet(array)
   array.collect do |name|
   name.capitalize + "!"
-end
+  end
 end
 
 def long_planeteer_calls(array)
-  
+  array.any? do |word|
+    word.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
